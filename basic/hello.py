69ed6778cbce2831820b2c21df5bcd18b8f41331
phantomve <francisco_cas14@hotmail.com>
2023-11-26 20:23:45 -0400

    feat: adding new file hello.py

---
+++ basic/hello.py
@@ -0,0 +1,24 @@
+# Esto es un comentario de  una linea Hola Mundo
+"""
+Este es un
+comnetario multilinea
+"""
+#Tipo de datos String
+print("Hola Mundo")
+print('Hola Mundo')
+
+'''
+Este es otro comentario
+de varias líneas
+'''
+
+"""Type es una operacion del sistema que nos
+indica el tipo de dato al que estamos haciendo
+referencia"""
+
+# Consultanto el tipo de dato 
+print(type("hola Mundo"))   # Tipo de dato 'str'
+print(type(5))              # Tipo de dato int
+print(type(1.5))            # Tipo de dato float
+print(type(3 + 2j))         # Tipo de dato 'complex'
+print(type(True))           # Tipo de dato boolean
